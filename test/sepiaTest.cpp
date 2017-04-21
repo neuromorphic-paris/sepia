0c669734d54186b48613c3d9ff281f2c542b36e1
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2017-04-21 17:50:22 +0200

    Refactor the interface with respect to the 1.0 Event Stream specification.
    Breaking changes:
    - ChangeDetection renamed to DvsEvent
    - Event renamed to AtisEvent
    - EventStreamObservable renamed to AtisEventStreamObservable
    - Parameters ordering in event stream observables

--- test/sepiaTest.cpp
+++ test/sepiaTest.cpp
@@ -4,21 +4,65 @@
 
 #include <mutex>
 
-TEST_CASE("Event counter", "[sepia]") {
+TEST_CASE("DVS event counter", "[AtisEventStreamObservable]") {
     std::size_t count = 0;
     std::exception_ptr sharedException;
     std::mutex lock;
     lock.lock();
     try {
-        auto camera = sepia::make_eventStreamObservable(
-            "../../test/sepiaTest.es",
-            [&](sepia::Event) -> void {
+        auto camera = sepia::make_dvsEventStreamObservable(
+            "../../test/sepiaDvsTest.es",
+            [&](sepia::DvsEvent) -> void {
                 ++count;
             },
-            [&](std::exception_ptr exception) {
+            [&](std::exception_ptr exception) -> void {
                 sharedException = exception;
                 lock.unlock();
-            }
+            },
+            []() -> bool {
+                return false;
+            },
+            sepia::EventStreamObservable::Dispatch::asFastAsPossible
+        );
+        lock.lock();
+        lock.unlock();
+        if (sharedException) {
+            std::rethrow_exception(sharedException);
+        }
+    } catch (const sepia::EndOfFile&) {
+        if (count != 2418241) {
+            FAIL("the event stream observable generated an unexpected number of events (expected 2418241, got " + std::to_string(count) + ")");
+        }
+    } catch (const std::runtime_error& exception) {
+        FAIL(exception.what());
+    }
+}
+
+
+TEST_CASE("ATIS event counter", "[AtisEventStreamObservable]") {
+    std::size_t count = 0;
+    std::exception_ptr sharedException;
+    std::mutex lock;
+    lock.lock();
+    try {
+        auto camera = sepia::make_atisEventStreamObservable(
+            "../../test/sepiaAtisTest.es",
+            sepia::make_split(
+                [&](sepia::DvsEvent) -> void {
+                    ++count;
+                },
+                [&](sepia::ThresholdCrossing) -> void {
+                    ++count;
+                }
+            ),
+            [&](std::exception_ptr exception) -> void {
+                sharedException = exception;
+                lock.unlock();
+            },
+            []() -> bool {
+                return false;
+            },
+            sepia::EventStreamObservable::Dispatch::asFastAsPossible
         );
         lock.lock();
         lock.unlock();
@@ -27,7 +71,7 @@ TEST_CASE("Event counter", "[sepia]") {
         }
     } catch (const sepia::EndOfFile&) {
         if (count != 2649650) {
-            FAIL("the event stream observable generated an unexpected number of events (expected 200, got " + std::to_string(count) + ")");
+            FAIL("the event stream observable generated an unexpected number of events (expected 2649650, got " + std::to_string(count) + ")");
         }
     } catch (const std::runtime_error& exception) {
         FAIL(exception.what());
