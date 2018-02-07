e8552ed6903504ed24738a1abfd887742b3d876b
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2018-02-07 16:45:19 +0100

    Refactor sepia to use snake_case
    Former-commit-id: f896711d057f960348034f53b0dcca445906e96c

--- premake4.lua
+++ premake4.lua
@@ -1,58 +1,20 @@
 solution 'sepia'
-    configurations {'Release', 'Debug'}
+    configurations {'release', 'debug'}
     location 'build'
-
-    newaction {
-        trigger = "install",
-        description = "Install the library",
-        execute = function()
-            os.copyfile('source/sepia.hpp', '/usr/local/include/sepia.hpp')
-            print(string.char(27) .. '[32mSepia library installed.' .. string.char(27) .. '[0m')
-            os.exit()
-        end
-    }
-
-    newaction {
-        trigger = 'uninstall',
-        description = 'Remove all the files installed during build processes',
-        execute = function()
-            os.execute('rm -f /usr/local/include/sepia.hpp')
-            print(string.char(27) .. '[32mSepia library uninstalled.' .. string.char(27) .. '[0m')
-            os.exit()
-        end
-    }
-
-    project 'sepiaTest'
-
-        -- General settings
+    project 'test'
         kind 'ConsoleApp'
         language 'C++'
         location 'build'
-        files {'source/**.hpp', 'test/**.hpp', 'test/**.cpp'}
-
-        -- Define the include paths
-        includedirs {'/usr/local/include'}
-        libdirs {'/usr/local/lib'}
-
-        -- Declare the configurations
-        configuration 'Release'
-            targetdir 'build/Release'
+        files {'source/*.hpp', 'test/*.cpp'}
+        buildoptions {'-std=c++11'}
+        linkoptions {'-std=c++11'}
+        configuration 'release'
+            targetdir 'build/release'
             defines {'NDEBUG'}
             flags {'OptimizeSpeed'}
-        configuration 'Debug'
-            targetdir 'build/Debug'
+        configuration 'debug'
+            targetdir 'build/debug'
             defines {'DEBUG'}
             flags {'Symbols'}
-
-        -- Linux specific settings
         configuration 'linux'
-            buildoptions {'-std=c++11'}
-            linkoptions {'-std=c++11'}
             links {'pthread'}
-            postbuildcommands {'cp ../source/sepia.hpp /usr/local/include/sepia.hpp'}
-
-        -- Mac OS X specific settings
-        configuration 'macosx'
-            buildoptions {'-std=c++11', '-stdlib=libc++'}
-            linkoptions {'-std=c++11', '-stdlib=libc++'}
-            postbuildcommands {'cp ../source/sepia.hpp /usr/local/include/sepia.hpp'}
