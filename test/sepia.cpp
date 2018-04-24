61b395485af7d69a9fcc1f9ca071a27d40a56b31
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2018-04-24 11:15:30 +0200

    Update sepia to the Event Stream spec 2.0
    Former-commit-id: 2d523c0c5edb663cbf32d3e5097f70697115adfe

--- test/sepia.cpp
+++ test/sepia.cpp
@@ -3,21 +3,21 @@
 #include "../third_party/Catch2/single_include/catch.hpp"
 
 TEST_CASE("Read DVS header type", "[sepia::read_type]") {
-    REQUIRE(sepia::read_type("../../test/dvs.es") == sepia::event_stream_type::dvs);
+    REQUIRE(sepia::read_header("../../test/dvs.es").event_stream_type == sepia::event_stream_type::dvs);
 }
 
 TEST_CASE("Read ATIS header type", "[sepia::read_type]") {
-    REQUIRE(sepia::read_type("../../test/atis.es") == sepia::event_stream_type::atis);
+    REQUIRE(sepia::read_header("../../test/atis.es").event_stream_type == sepia::event_stream_type::atis);
 }
 
 TEST_CASE("Read color header type", "[sepia::read_type]") {
-    REQUIRE(sepia::read_type("../../test/color.es") == sepia::event_stream_type::color);
+    REQUIRE(sepia::read_header("../../test/color.es").event_stream_type == sepia::event_stream_type::color);
 }
 
 TEST_CASE("Count DVS events", "[sepia::dvs_event_stream_observable]") {
     std::size_t count = 0;
     sepia::join_dvs_event_stream_observable("../../test/dvs.es", [&](sepia::dvs_event) -> void { ++count; });
-    if (count != 2418241) {
+    if (count != 476203) {
         FAIL(
             "the event stream observable generated an unexpected number of events (expected 2418241, got "
             + std::to_string(count) + ")");
@@ -27,7 +27,7 @@ TEST_CASE("Count DVS events", "[sepia::dvs_event_stream_observable]") {
 TEST_CASE("Count ATIS events", "[sepia::atis_event_stream_observable]") {
     std::size_t count = 0;
     sepia::join_atis_event_stream_observable("../../test/atis.es", [&](sepia::atis_event) -> void { ++count; });
-    if (count != 2649650) {
+    if (count != 1428204) {
         FAIL(
             "the event stream observable generated an unexpected number of events (expected 2418241, got "
             + std::to_string(count) + ")");
@@ -37,7 +37,7 @@ TEST_CASE("Count ATIS events", "[sepia::atis_event_stream_observable]") {
 TEST_CASE("Count color events", "[sepia::color_event_stream_observable]") {
     std::size_t count = 0;
     sepia::join_color_event_stream_observable("../../test/color.es", [&](sepia::color_event) -> void { ++count; });
-    if (count != 2839574) {
+    if (count != 976510) {
         FAIL(
             "the event stream observable generated an unexpected number of events (expected 2839574, got "
             + std::to_string(count) + ")");
