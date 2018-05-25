12e9ba1236c229d534515993d8c0fd483aa8ada9
Alexandre Marcireau <alexandre.marcireau@inserm.fr>
2018-05-25 14:02:09 +0200

    Debug Travis

--- test/sepia.cpp
+++ test/sepia.cpp
@@ -6,6 +6,9 @@
 const std::string examples =
     sepia::join({sepia::dirname(sepia::dirname(__FILE__)), "third_party", "event_stream", "examples"});
 
+#include <iostream> // @DEBUG
+std::cout << "running examples from " << __FILE__ << std::endl; // @DEBUG
+
 TEST_CASE("read generic header type", "[sepia::read_type]") {
     REQUIRE(
         sepia::read_header(sepia::filename_to_ifstream(sepia::join({examples, "generic.es"}))).event_stream_type
